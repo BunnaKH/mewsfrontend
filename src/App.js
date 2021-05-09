a025eff58f4c0ad8e513d60c6491d4952b913fa4
BunnaKH <bunnaq7@gmail.com>
2021-05-09 21:29:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import {
+  BrowserRouter as Router, Route,
+  Link
+
+} from "react-router-dom";
+import MovieDetail from './Components/MovieDetail';
+import MovieList from './Components/MovieList';
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+const AppStyle = styled.div`
+color:#fff;
+text-align:center;
+`
+const Title = styled.h1`
+color:#fff;
+
+`;
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <AppStyle>
+          <Link to='/'><Title>THE MOVIE DB</Title></Link>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movie/:id" component={MovieDetail} />
+        </AppStyle>
+      </Router>
+
+    </Provider>
+
+
+
   );
 }
 
